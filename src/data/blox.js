3408d23c97f1608bdbe1a1f29b231f6ce362457f
Brian Ketelsen <bketelsen@gmail.com>
2022-06-08 14:30:20 -0400

    change some routes

--- src/data/blox.js
+++ src/data/blox.js
@@ -1,7 +1,7 @@
 import data from './data.json';
 
-export function getProfile(profile) {
-    return data.profiles.find(p => p.id === profile);
+export function getProfile(id) {
+    return data.profiles.find(p => p.id === id);
 }
 export function getArticle(id) {
     return data.articles.find(p => p.id === id);
