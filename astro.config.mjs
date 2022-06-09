14ebb9b7d347eb85f576f0ef559a5d2dd636e675
Brian Ketelsen <bketelsen@gmail.com>
2022-06-09 11:24:32 -0400

    add compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,25 +4,28 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
 import svelte from "@astrojs/svelte";
+import robotsTxt from 'astro-robots-txt';
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig(
-	/** @type {import('astro').AstroUserConfig} */
-	{
-		markdown: {
-			render: [renderMarkdown, {
-				remarkPlugins: [],
-				rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
-					behavior: 'wrap'
-				}]],
-				shikiConfig: {
-					theme: 'poimandres',
-					langs: [],
-					wrap: false
-				}
-			}]
-		},
-		site: 'https://brian.dev',
-
-		integrations: [astroImageTools,svelte(),sitemap(),tailwind()]
-	});
+/** @type {import('astro').AstroUserConfig} */
+{
+  markdown: {
+    render: [renderMarkdown, {
+      remarkPlugins: [],
+      rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
+        behavior: 'wrap'
+      }]],
+      shikiConfig: {
+        theme: 'poimandres',
+        langs: [],
+        wrap: false
+      }
+    }]
+  },
+  site: 'https://brian.dev',
+  integrations: [astroImageTools, svelte(), sitemap(), tailwind(), robotsTxt(), compress({			img: false,
+  })]
+});
