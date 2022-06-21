6411464c2121670a1a5b7fc75d1557a299efbf29
Brian Ketelsen <bketelsen@gmail.com>
2022-06-21 09:12:37 +0000

    chore: update post header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
 module.exports = {
-    content: ['./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}'],
-    plugins: [
-        require('@tailwindcss/typography'),
-        require("@tailwindcss/line-clamp"),
-      require('daisyui'),
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-            body: '"Inter", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
-            title: '"Poppins", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
-            heading: '"Poppins", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
-            sans: '"Poppins", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
-            serif: 'ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
-            mono: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
-        },
-      }
-    },
-    daisyui: {
-      themes: [
-        {
-          coffee: {
-            ...require("daisyui/src/colors/themes")["[data-theme=coffee]"],
-            "base-content": "#f8f1e7",
-          },
-        },
-      ],
+  content: ['./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}'],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require("@tailwindcss/line-clamp"),
+    require('daisyui'),
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: '"Inter", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+        title: '"Poppins", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+        heading: '"Poppins", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+        sans: '"Poppins", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+        serif: 'ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
+        mono: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+      },
     }
+  },
+  daisyui: {
+    themes: [
+      {
+        coffee: {
+          ...require("daisyui/src/colors/themes")["[data-theme=coffee]"],
+          "base-content": "#f8f1e7",
+        },
+      },
+    ],
   }
+}
